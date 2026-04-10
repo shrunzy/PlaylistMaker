e0b932e45cccf8704858ee507f1df3d985469c76
shrunzy <sh.runzy@gmail.com>
2026-04-10 09:28:56 -0700

    спринт 9 задание 1 - добавлено переключение темы по свичу

--- app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -3,6 +3,8 @@ package com.practicum.playlistmaker
 import android.os.Bundle
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import com.google.android.material.switchmaterial.SwitchMaterial
 
 class SettingsActivity : AppCompatActivity() {
 
@@ -10,6 +12,18 @@ class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
+        val switchDarkTheme: SwitchMaterial = findViewById(R.id.switch_notifications)
+        switchDarkTheme.isChecked = AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES
+        switchDarkTheme.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                // Включаем тёмную тему
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            } else {
+                // Выключаем (светлая тема)
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+        }
 
         findViewById<ImageView>(R.id.settings_back).setOnClickListener {
             finish()
